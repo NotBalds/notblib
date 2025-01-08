1c2ef0655f42d1e03915e4ae699b938a3d8ba15f
TBWanderer <evgenypodyryaka@yandex.ru>
2025-01-09 00:19:42 +0300

    Added some comments

--- src/crypt/elliptic.rs
+++ src/crypt/elliptic.rs
@@ -4,11 +4,13 @@ pub use ecies::{decrypt, encrypt};
 pub type SecretKey = [u8; 32];
 pub type PublicKey = [u8; 65];
 
+/// Structure of key for Elliptic curve cryptography (ECC)
 pub struct Key {
     pub secret: SecretKey,
     pub public: PublicKey,
 }
 
+/// Generates new keypair for Elliptic curve cryptography (ECC)
 pub fn gen_keys() -> Key {
     let (sk, pk) = generate_keypair();
     let (sk, pk) = (&sk.serialize(), &pk.serialize());
