0b7f3425ffb3bd2e56601a5a3a9c2554fe85e706
TBWanderer <evgenypodyryaka@yandex.ru>
2025-01-09 00:40:25 +0300

    Moved test AES-256 to tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -8,3 +8,16 @@ fn status_test() {
     status::error("Test error status");
     status::fatal("Test fatal status");
 }
+
+#[test]
+fn aes256_test() -> Result<(), super::AnyError> {
+    use crate::crypt::aes_256::*;
+
+    let key = Key::gen();
+    let data = b"Hello, world!";
+
+    let encrypted_data = encrypt(&key, data.to_vec())?;
+    let decrypted_data = decrypt(&key, &encrypted_data)?;
+    assert_eq!(decrypted_data, data);
+    Ok(())
+}
